f999954ecf956aa96bf4f51cd2ea7fde7ee4b269
Dawit Asmero <76691105+dawitasmeroatinafu@users.noreply.github.com>
2022-08-10 16:52:31 +0300

    some change

--- tests/test_extract_dataframe.py
+++ tests/test_extract_dataframe.py
@@ -12,7 +12,7 @@ from extract_dataframe import TweetDfExtractor
 # Create a sample not more than 10 tweets and place it in a json file.
 # Provide the path to the samples tweets file you created below
 sampletweetsjsonfile = ""   #put here the path to where you placed the file e.g. ./sampletweets.json. 
-_, tweet_list = read_json(sampletweetsjsonfile)
+_, tweet_list = read_json("data/global_tiwitter_data.json")
 
 columns = [
     "created_at",
@@ -45,7 +45,7 @@ class TestTweetDfExtractor(unittest.TestCase):
         -----
 			unittest.TestCase this allows the new class to inherit
 			from the unittest module
-	"""
+	 doing some changes here"""
 
     def setUp(self) -> pd.DataFrame:
         self.df = TweetDfExtractor(tweet_list[:5])
@@ -97,4 +97,3 @@ class TestTweetDfExtractor(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-
