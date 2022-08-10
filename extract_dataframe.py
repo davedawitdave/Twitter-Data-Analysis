3969dd0bb63718a0dd5156e29a04262493389a6a
Dawit Asmero <76691105+dawitasmeroatinafu@users.noreply.github.com>
2022-08-10 13:45:22 +0300

    add data to read json

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -162,7 +162,7 @@ class TweetDfExtractor:
         
         
     def get_tweet_df(self, save=False)->pd.DataFrame:
-        """required column to be generated you should be creative and add more features"""
+        """required column to be generated you should be creative and add more features"""     #OK 
         
         columns = ['created_at', 'source', 'original_text','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count', 
             'original_author', 'followers_count','friends_count','possibly_sensitive', 'hashtags', 'user_mentions', 'place']
@@ -195,7 +195,7 @@ if __name__ == "__main__":
     # required column to be generated you should be creative and add more features
     columns = ['created_at', 'source', 'original_text','clean_text', 'sentiment','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count', 
     'original_author', 'screen_count', 'followers_count','friends_count','possibly_sensitive', 'hashtags', 'user_mentions', 'place', 'place_coord_boundaries']
-    _, tweet_list = read_json("../covid19.json")
+    _, tweet_list = read_json("../data/global_twitter_data.json")
     tweet = TweetDfExtractor(tweet_list)
     tweet_df = tweet.get_tweet_df() 
 
