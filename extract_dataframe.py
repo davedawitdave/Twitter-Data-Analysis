50f9f6b394ff4d685dadc11af183018e32d42c50
Dawit Asmero <76691105+dawitasmeroatinafu@users.noreply.github.com>
2022-08-12 19:42:22 +0300

    extracted_data_debugged

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -38,10 +38,10 @@ class TweetDfExtractor:
   
     # an example function
     def find_statuses_count(self)->list:
-        statuses_count= []  # count the statuses of
+        statuses= []  # count the statuses of
         for items in self.tweets_list:
-            statuses_count.append(items['user']['status_count'])        
-        return statuses_count
+            statuses.append(items['user']['statuses_count'])      
+        return statuses
 
         
     def find_full_text(self)->list:
@@ -162,12 +162,13 @@ class TweetDfExtractor:
     def get_tweet_df(self, save=False)->pd.DataFrame:
         """required column to be generated you should be creative and add more features"""     #OK 
         
-        columns = ['created_at', 'source', 'original_text','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count', 
-            'original_author', 'followers_count','friends_count','possibly_sensitive', 'hashtags', 'user_mentions', 'place']
+        columns = ['created_at', 'source', 'original_text','clean_text','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count', 
+            'original_author', 'followers_count','friends_count','possibly_sensitive', 'hashtags','statuses_count', 'user_mentions', 'place']
         
         created_at = self.find_created_time()
         source = self.find_source()
-        text = self.find_full_text()
+        text = self.find_full_text()     
+        u_text = self.find_full_text()
         polarity, subjectivity = self.find_sentiments(text)
         lang = self.find_lang()
         fav_count = self.find_favourite_count()
@@ -177,9 +178,10 @@ class TweetDfExtractor:
         friends_count = self.find_friends_count()
         sensitivity = self.is_sensitive()
         hashtags = self.find_hashtags()
+        statuses_count = self.find_statuses_count()
         mentions = self.find_mentions()
         location = self.find_location()
-        data = zip(created_at, source, text, polarity, subjectivity, lang, fav_count, retweet_count, screen_name, follower_count, friends_count, sensitivity, hashtags, mentions, location)
+        data = zip(created_at, source, text, u_text, polarity, subjectivity, lang, fav_count, retweet_count, screen_name, follower_count, friends_count, sensitivity, hashtags,statuses_count, mentions, location)
         df = pd.DataFrame(data=data, columns=columns)
 
         if save:
